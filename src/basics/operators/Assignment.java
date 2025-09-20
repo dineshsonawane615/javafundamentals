444c25bb031e74913fdce989c996426581a71845
dineshsonawane615 <dineshsonawane615@gmail.com>
2025-09-20 16:36:22 +0530

    operators

---
+++ src/basics/operators/Assignment.java
@@ -0,0 +1,15 @@
+package basics.operators;
+
+public class Assignment {
+    public static void main(String[] args) {
+        int myInt = 9;
+        System.out.println(myInt);
+        int newInt = myInt;
+        System.out.println(newInt);
+
+        int a = 10;
+        a += 5; // a = a + 5 → 15
+        System.out.println(a);
+
+    }
+}
---
+++ src/basics/operators/unary.java
@@ -0,0 +1,23 @@
+package basics.operators;
+
+public class unary {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = -a;
+        int c = -b;
+        System.out.println(c);
+
+        int x = 5;
+        x = x + 1;
+        System.out.println(x);
+        x += 1;
+        System.out.println(x);
+        x++;
+        System.out.println(x);
+
+//        System.out.println("Watching increment" );
+//        System.out.println(x++);
+//        System.out.println(x);
+//        System.out.println(++x);   //mostly confusion occurs
+    }
+}
